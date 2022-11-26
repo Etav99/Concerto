ffb7f95194cf104635da9472a7f7cf69d20fc92c
Maciej Zakrzewski <etavpl@gmail.com>
2022-11-26 21:48:45 +0100

    Add new course member picker

--- Shared/Models/ViewModels/Course.cs
+++ Shared/Models/ViewModels/Course.cs
@@ -1,4 +1,6 @@
-﻿namespace Concerto.Shared.Models.Dto;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Concerto.Shared.Models.Dto;
 
 public record Course(
     long Id,
@@ -14,12 +16,43 @@ public record CourseUser(long UserId, CourseUserRole Role)
 {
     public CourseUserRole Role { get; set; } = Role;
 }
+
+public class CourseUserIdEqualityComparer : IEqualityComparer<CourseUser>
+{
+    public bool Equals(CourseUser? x, CourseUser? y)
+    {
+        return x?.UserId == y?.UserId;
+    }
+
+    public int GetHashCode([DisallowNull] CourseUser obj)
+    {
+        return obj.UserId.GetHashCode();
+    }
+}
+
 public enum CourseUserRole
 {
     Admin = 0,
     Supervisor = 1,
     Member = 2,
 }
+
+public static class CourseUserRoleExtensions
+{
+    public static string ToDisplayString(this CourseUserRole role)
+    {
+        return role switch
+        {
+            CourseUserRole.Admin => "Administrator",
+            CourseUserRole.Supervisor => "Supervisor",
+            CourseUserRole.Member => "Member",
+            _ => throw new ArgumentOutOfRangeException(nameof(role), role, null)
+        };
+    }
+}
+
+
+
 
 public record CreateCourseRequest
 {
